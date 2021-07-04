39f233447e2504975a91b3c570ad2974a1fefebc
Maria <83551206+maria-nlp@users.noreply.github.com>
2021-07-04 23:53:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = ‘/app/.apt/usr/bin/tesseract’
 from PIL import Image
 import requests
 
