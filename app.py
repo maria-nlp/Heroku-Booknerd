7ec6f9cbf2b9182c19fd679a65323d972994639f
Maria <83551206+maria-nlp@users.noreply.github.com>
2021-07-04 17:41:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,31 +8,22 @@ import cv2
 
 app = Flask(__name__)
 
-api_key = "AIzaSyDlp19ogXeUuugzO3UZYRUqL9RVSzo2nQk"
-
-@app.route('/getbook', methods=["POST"])
-
+@app.route("/", methods=["POST"])
 def getbooktitle():
-    file = request.files['image']
-    img = Image.open(file.stream)
-    booktitle = pytesseract.image_to_string(img)
+    if request.method == "POST":
+        file = request.files['image']
+        img = Image.open(file.stream)
+        booktitle = pytesseract.image_to_string(img)
 
-    url = f"https://www.googleapis.com/books/v1/volumes?q={booktitle}&key={api_key}"
-    response = requests.get(url)
-    json_response = response.json()
-    volume_info = json_response['items'][0]['volumeInfo']
-    title = volume_info.get('title')
-    author = volume_info.get('authors')
-    images_link = volume_info['imageLinks']
-    #small_image = images_link['smallThumbnail']
-    data = {
-        'title': title,
-        'author': author,
-        'images_links': images_link
-    }
-    data_in_json = json.dumps(data)
-
-    return data_in_json
+        api_key = "AIzaSyDlp19ogXeUuugzO3UZYRUqL9RVSzo2nQk"
+        url = f"https://www.googleapis.com/books/v1/volumes?q={booktitle}&key={api_key}"
+        response = requests.get(url)
+        json_response = response.json()
+        volume_info = json_response['items'][0]['volumeInfo']
+        title = volume_info.get('title')
+        author = volume_info.get('authors')
+        images_link = volume_info['imageLinks']
+        return jsonify({'title': title,'author': author,'images_links': images_link})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
