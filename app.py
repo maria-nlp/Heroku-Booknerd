762c1e9bf04fe9e7dec3abf5b5274306eb954d90
piushaggarwal <piush.aggarwal@uni-due.de>
2021-07-16 16:28:05 +0200

    change aptfile

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = '/app/vendor/tesseract-ocr/bin/tesseract'
 from PIL import Image
 import requests
 
