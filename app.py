c684ff163d6a9301376cafc3c3500aefdd1a4b5d
Maria <83551206+maria-nlp@users.noreply.github.com>
2021-07-23 12:26:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/getbook", methods=["POST"])
+@app.route("/imgt", methods=["POST", "GET"])
 def getbooktitle():
     if request.method == "POST":
         file = request.files['image']
